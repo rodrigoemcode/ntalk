64499862c589db5797627f7ab83442cb52c00520
Rodrigo <rodrigosilva.emcode@gmail.com>
2022-12-05 18:16:36 -0300

    Criação do Ntalk

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express')
+  , routes = require('./routes');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', routes.index);
+app.get('/about', routes.unsubscribe.index);
+
+app.listen(3000, function () {
+  console.log("Ntalkno ar.");
+});
+
