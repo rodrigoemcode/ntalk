7926abea13b040b7d680e7e2ba6d493f568504e4
Rodrigo <rodrigosilva.emcode@gmail.com>
2022-12-08 07:53:43 -0300

    refactoing app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express')
-  , routes = require('./routes');
-var app = express();
+  , load = require('express-load')
+  // , routes = require('./routes');
+  , app = express();
+
+// ...stack de configurações do servidor...
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -8,6 +11,13 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', routes.index);
 app.get('/about', routes.unsubscribe.index);
+
+load('models')
+    .then('controllers')
+    .then('routes')
+    .into(app);
+
+// ...app.listen(3000)...
 
 app.listen(3000, function () {
   console.log("Ntalk no ar.");
